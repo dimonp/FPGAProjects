8db522683840b3e177f2596c450ef048ec7c961f
Dmitry Peter <dmitrypeter@gmail.com>
2018-10-20 17:06:39 +0600

    Decomposite Snake module. Use keyboard to control.

--- My_VHDL_Testbench/SnakeGame/src/hdl/snake_game_top.vhd
+++ My_VHDL_Testbench/SnakeGame/src/hdl/snake_game_top.vhd
@@ -7,10 +7,8 @@ entity Snake_Game_top is
     port(
         clk         : in std_logic;
         rst         : in std_logic;
-        keyUp       : in std_logic;
-        keyDown     : in std_logic;
-        keyLeft     : in std_logic;
-        keyRight    : in std_logic;
+        ps2Data     : in std_logic;
+        ps2Clock    : in std_logic;
         vgaHs       : out std_logic;
         vgaVs       : out std_logic;
         vgaR        : out std_logic_vector(4 downto 0);
@@ -28,21 +26,14 @@ architecture behavioral of Snake_Game_top is
     constant DISPLAY_WIDTH : natural := 80;
     constant DISPLAY_HEIGHT : natural := 30;
 
-    type t_Snake_state is (sRunUp, sRunDown, sRunLeft, sRunRight, sStop);
-    type t_Draw_state is (sHead, sBody, sTail);
-
     signal wen      : std_logic := '0';
     signal addr     : std_logic_vector (11 downto 0);
     signal data     : std_logic_vector(15 downto 0);
     signal q        : std_logic_vector(23 downto 0) := (others => '0');
     signal isDone   : boolean := false;
-
-    type t_Coords is record
-        xc : natural range 0 to DISPLAY_WIDTH-1;
-        yc : natural range 0 to 39;
-    end record;
-    type t_Coords_fifo is array(0 to FIFO_SIZE-1) of t_Coords;
-    signal fifo : t_Coords_fifo := (others=>(others=>0));
+    signal ps2Code  : std_logic_vector(7 downto 0);
+    signal snake_xc : natural range 0 to DISPLAY_WIDTH - 1;
+    signal snake_yc : natural range 0 to DISPLAY_HEIGHT - 1;
 
     function calcAddr(x : natural range 0 to DISPLAY_WIDTH-1;
                       y : natural range 0 to DISPLAY_HEIGHT-1) 
@@ -50,18 +41,6 @@ architecture behavioral of Snake_Game_top is
     begin
         return std_logic_vector(to_unsigned(y * 80 + x, 12));
     end function calcAddr;
-
-    procedure updateFifo(x : natural range 0 to DISPLAY_WIDTH-1;
-                         y : natural range 0 to DISPLAY_HEIGHT-1) is
-    begin
-        -- This loop is unrolled by the synthesis tool.
-        for i in FIFO_SIZE-1 downto 1 loop
-            fifo(i) <= fifo(i-1);
-        end loop;
-
-        -- insert into position zero
-        fifo(0) <= (xc=>x, yc=>y);
-    end procedure updateFifo;
 
     component VGA_text
         port (
@@ -76,7 +55,28 @@ architecture behavioral of Snake_Game_top is
             g       : out std_logic_vector(5 downto 0);
             b       : out std_logic_vector(4 downto 0));
     end component;
+    
+    component ps2_keyboard
+        port(
+            clk          : in  std_logic;
+            ps2_clk      : in  std_logic;
+            ps2_data     : in  std_logic;
+            ps2_code_new : out std_logic;
+            ps2_code     : out std_logic_vector(7 downto 0));
+    end component;
+
+    component Snake
+        port(
+            i_clk     : in  std_logic;
+            i_rst     : in  std_logic;
+            i_ps2Code : in  std_logic_vector(7 downto 0);
+            o_xc      : out natural range 0 to DISPLAY_WIDTH - 1;
+            o_yc      : out natural range 0 to DISPLAY_HEIGHT - 1;
+            o_data    : out std_logic_vector(15 downto 0));
+    end component;
 begin
+    addr <= calcAddr(snake_xc, snake_yc);
+
     vgaText_inst : VGA_text port map (
             clock => clk,
             reset => not rst,
@@ -89,125 +89,29 @@ begin
             g => vgaG,
             b => vgaB);
 
+    keyboardPs2_inst: component ps2_keyboard
+        port map(
+            clk          => clk,
+            ps2_clk      => ps2Clock,
+            ps2_data     => ps2Data,
+            ps2_code_new => open,
+            ps2_code     => ps2Code
+        );
+        
+    snake_inst: component Snake
+        port map(
+            i_clk     => q(20),
+            i_rst     => not rst,
+            i_ps2Code => ps2Code,
+            o_xc      => snake_xc,
+            o_yc      => snake_yc,
+            o_data    => data
+        );
+     
     process(clk) is
     begin
         if rising_edge(clk) then
             q <= q + 1;
-        end if;
-    end process;
-
-    -- State Machine
-    -- 6 Hz
-    process (q(22), rst)
-        variable x      : natural range 0 to DISPLAY_WIDTH-1 := 0;
-        variable y      : natural range 0 to DISPLAY_HEIGHT-1 := 0;
-        variable state  : t_Snake_state := sStop;
-        variable delta_x : integer;
-        variable delta_y : integer;
-
-        procedure changeState(currentState : t_Snake_state) is
-        begin
-            if keyUp = '0' and state /= sRunDown then
-                state := sRunUp;
-            elsif keyDown = '0' and state /= sRunUp then
-                state := sRunDown;
-            elsif keyLeft = '0' and state /= sRunRight then
-                state := sRunLeft;
-            elsif keyRight = '0' and state /= sRunLeft then
-                state := sRunRight;
-            end if;
-        end procedure changeState;
-
-        function updateX(x      : natural range 0 to DISPLAY_WIDTH-1; 
-                         delta  : integer) 
-                return natural is
-            variable tmp_x : integer;
-        begin
-            tmp_x := x + delta;
-
-            if tmp_x > DISPLAY_WIDTH-1 then
-                tmp_x := 0;
-            elsif tmp_x < 0 then
-                tmp_x := DISPLAY_WIDTH-1;
-            end if;
-    
-            return tmp_x;
-        end function updateX;
-
-        function updateY(y      : natural range 0 to DISPLAY_HEIGHT-1; 
-                         delta  : integer) 
-                return natural is
-            variable tmp_y : integer;
-        begin
-            tmp_y := y + delta;
-
-            if tmp_y > DISPLAY_HEIGHT-1 then
-                tmp_y := 0;
-            elsif tmp_y < 0 then
-                tmp_y := DISPLAY_HEIGHT-1;
-            end if;
-    
-            return tmp_y;
-        end function updateY;
-
-    begin   
-        if rst = '0' then
-            x := 0;
-            y := 0;
-            state := sStop;
-            isDone <= false;
-        elsif rising_edge(q(22)) then
-            changeState(state);
-
-            case state is
-                when sRunUp =>
-                    delta_x := 0;
-                    delta_y := -1;
-                when sRunDown =>
-                    delta_x := 0;
-                    delta_y := 1;
-                when sRunLeft =>
-                    delta_x := -1;
-                    delta_y := 0;
-                when sRunRight =>
-                    delta_x := 1;
-                    delta_y := 0;
-                when sStop =>
-                    delta_x := 1;
-                    delta_y := 0;
-            end case;
-
-            x := updateX(x, delta_x);
-            y := updateY(y, delta_y);
-
-            updateFifo(x, y);
-        end if;
-    end process;
-
-    -- 24 Hz
-    process(q(20))
-        variable state      : t_Draw_State := sHead;
-        variable drawAddr   : std_logic_vector (11 downto 0);
-        variable drawData   : std_logic_vector(15 downto 0);
-    begin
-        if rising_edge(q(20)) then
-            case state is
-                when sHead =>
-                    drawAddr := calcAddr(fifo(0).xc, fifo(0).yc);
-                    drawData := "0000001000000010";
-                    state := sBody;
-                when sBody =>
-                    drawAddr := calcAddr(fifo(1).xc, fifo(1).yc);
-                    drawData := "0000001000000001";
-                    state := sTail;
-                when sTail =>
-                    drawAddr := calcAddr(fifo(FIFO_SIZE-1).xc, fifo(FIFO_SIZE-1).yc);
-                    drawData := (others=>'0');
-                    state := sHead;
-            end case;
-
-            addr <= drawAddr;
-            data <= drawData;
         end if;
     end process;
 
